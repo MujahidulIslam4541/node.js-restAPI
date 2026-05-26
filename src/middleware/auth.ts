976fc63a71d50c4e5ab9f9c189e2fe90ed7ac555
MujahidulIslam4541 <mirifat4541@gmail.com>
2026-05-27 05:26:32 +0600

    added token verification

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,6 +2,10 @@ import type { NextFunction, Request, Response } from "express";
 
 const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ success: false, message: "Unauthorized Access!" });
+  }
+  next();
 };
 
 export default authMiddleware;
